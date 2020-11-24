fab826738896ada8a735f97a51e0d5600298579d
Wesly Grefrath <wesly@eyo.nl>
2020-11-24 10:36:55 +0100

    Swap around some layer keys

--- keyboards/clueboard/66/keymaps/wezzynl/keymap.c
+++ keyboards/clueboard/66/keymaps/wezzynl/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,                   KC_PGDOWN, \
                  FUNC(0),  KC_A,   KC_S,   KC_D,   KC_F,           KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT, \
                  KC_LSFT, KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,           KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,  MO(_FL),          KC_UP,            \
-                 TT(_ML), MO(_GL), KC_LALT,KC_LGUI,        KC_ENT, KC_SPC,                         TT(_ML), KC_RGUI,  MO(_FL), KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT),
+                 TT(_ML), MO(_GL), KC_LALT,KC_LGUI,        KC_ENT, KC_SPC,                         MO(_FL), KC_RGUI,  MO(_PL), KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Keymap _FL: Function Layer
    */
