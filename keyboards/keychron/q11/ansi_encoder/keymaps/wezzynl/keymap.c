77a80b8aa94546db94d526d06432f0afc2d92c8d
Wesly Grefrath <wesly@eyo.nl>
2025-01-15 12:22:24 +0100

    Add hjkl/m to CTL layer

--- keyboards/keychron/q11/ansi_encoder/keymaps/wezzynl/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/wezzynl/keymap.c
@@ -67,8 +67,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MUTE,  KC_ESC,                   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RM_VALD,   RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_INS,   KC_DEL,   KC_MUTE,
         _______,  LSFT(KC_TAB),             KC_1,     KC_2,       KC_3,   KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         _______,  KC_TAB,                   KC_Q,     KC_W,       KC_E,   KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
-        _______,  MT(MOD_LCTL, KC_ESCAPE),  KC_A,     KC_S,       KC_D,   KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
-        _______,  KC_LSFT,                  KC_Z,     KC_X,       KC_C,   KC_V,     KC_B,      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
+        _______,  MT(MOD_LCTL, KC_ESCAPE),  KC_A,     KC_S,       KC_D,   KC_F,     KC_G,      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
+        _______,  KC_LSFT,                  KC_Z,     KC_X,       KC_C,   KC_V,     KC_B,      KC_N,     KC_ENT,   KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
         _______,  KC_LCTL,                  KC_LOPT,  MO(MAC_FN), KC_LCMD,          KC_ENT,                        KC_SPC,             KC_RCMD,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_BASE] = LAYOUT_91_ansi(
