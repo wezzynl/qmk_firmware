2efc96671fcb3567c0582e6ae4c173a42cf46cdb
Wesly Grefrath <wesly@eyo.nl>
2025-01-15 16:08:59 +0100

    Add right alt

--- keyboards/keychron/q11/ansi_encoder/keymaps/wezzynl/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/wezzynl/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_TAB,                   KC_Q,     KC_W,       KC_E,   KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         _______,  MT(MOD_LCTL, KC_ESCAPE),  KC_A,     KC_S,       KC_D,   KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         _______,  KC_LSFT,                  KC_Z,     KC_X,       KC_C,   KC_V,     KC_B,      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        _______,  MO(MAC_FN),               KC_LCTL,  KC_LOPT,    KC_LCMD,          KC_ENT,                        KC_SPC,             KC_RCMD,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        _______,  MO(MAC_FN),               KC_LCTL,  KC_LOPT,    KC_LCMD,          KC_ENT,                        KC_SPC,             KC_RALT,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_91_ansi(
         QK_BOOT,  KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RM_VALD,   RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_INS,   KC_DEL,   QK_BOOT,
